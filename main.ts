bebc7994cc1b51644894ce117f9a02f0e1bb1f91
DigitalVision <46016928+josephsmi2019@users.noreply.github.com>
2020-05-19 20:17:58 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let projectile: Sprite = null
+game.onUpdate(function () {
+    if (Math.percentChance(25)) {
+        projectile = sprites.createProjectileFromSide(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 1 . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, 0, Math.randomRange(20, 30))
+        projectile.setPosition(Math.randomRange(0, scene.screenWidth()), 0)
+        projectile.setFlag(SpriteFlag.Ghost, true)
+    }
+})
